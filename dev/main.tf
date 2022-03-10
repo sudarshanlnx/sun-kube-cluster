eeb608cd5eebb2da822b071f1f8e9206040264a9
sudarshanlnx <87026085+sudarshanlnx@users.noreply.github.com>
2022-03-10 07:40:43 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -4,8 +4,8 @@ module "clustering" {
   vpcname     = var.vpcname
   pubsub01    = var.pubsub01
   pubsub02    = var.pubsub02
-  pri01       = var.pri01
-  pri02       = var.pri02
+  #pri01       = var.pri01
+  #pri02       = var.pri02
   block1      = var.block1
   block2      = var.block2
   block3      = var.block3
