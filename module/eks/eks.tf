c50ae41fe55d244b6a29513661d4a2230fb78c2f
sudarshanlnx <87026085+sudarshanlnx@users.noreply.github.com>
2023-03-15 08:42:58 +0530

    Update eks.tf

--- module/eks/eks.tf
+++ module/eks/eks.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_cluster" "global-cluster" {
   name = var.clustername
   role_arn = aws_iam_role.globalrole.arn
-  version = "1.20"
+  version = "1.24"
 
   vpc_config {
     subnet_ids = [aws_subnet.pubsub01.id, aws_subnet.pubsub02.id] #aws_subnet.pri01.id, aws_subnet.pri02.id]
