f2db62c56c912764a213f2c2ec0ea67efa1b263e
bathusha07 <96715718+bathusha07@users.noreply.github.com>
2024-06-29 21:57:03 +0300

    Update provider.tf

--- dev/provider.tf
+++ dev/provider.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.61.0"
+      version = "5.56.1"
     }
   }
 }
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
